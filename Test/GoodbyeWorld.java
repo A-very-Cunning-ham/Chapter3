ccb85ce29941694f62baa4b1f103f65223b77eda
Avery Cunningham <cunninghamavery@laceyschools.org>
2017-11-01 16:25:28 +0000

    first commit

---
+++ Test/GoodbyeWorld.java
@@ -0,0 +1,12 @@
+
+public class GoodbyeWorld
+{
+  public static void main(String[] args) {
+    
+    
+    System.out.println("Henlo Rabbit");
+    System.out.print("Naomi, I moan");
+    System.out.println("A man, a plan, a canal, Panama");
+    System.out.println(3 + 4 * 10);
+  }
+}
